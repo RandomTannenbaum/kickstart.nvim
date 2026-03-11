4ea4ed841d21e030715463c498e1ae406a412183
Jannik Pulfer <pulfer@puzzle.ch>
2026-03-11 15:43:43 +0100

    Add vim rails plugin and update packages

--- init.lua
+++ init.lua
@@ -880,6 +880,9 @@ require('lazy').setup({
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
   },
+  {
+    'tpope/vim-rails',
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
